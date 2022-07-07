cc1ee1654804ba4231d49a94630889a294aaea00
Dan Labbad <dan.labbad@mac.com>
2022-07-07 07:50:42 +0100

    Fixed password authentication

--- server/NewBank.java
+++ server/NewBank.java
@@ -3,35 +3,50 @@ package newbank.server;
 import java.util.HashMap;
 
 public class NewBank {
-	
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
-	
+	/**
+	 * Created nested HashMap to hold the Customer, username, and password
+	 * relationships:
+	 * HashMap<Customer, HashMap <String, String>> which maps to
+	 *  <Customer < userName, password>>
+	 **/
+	private HashMap< String, HashMap <Customer, String> > customers;
+
 	private NewBank() {
 		customers = new HashMap<>();
+
 		addTestData();
 	}
-	
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
-		customers.put("Bhagy", bhagy);
-		
+		/** put username, Customer and password into the HashMap for each customer */
+		customers.put("Bhagy", new HashMap(){{put(bhagy, "12345");}});
+
 		Customer christina = new Customer();
 		christina.addAccount(new Account("Savings", 1500.0));
-		customers.put("Christina", christina);
-		
+		/** put username, Customer and password into the HashMap for each customer */
+		customers.put("Christina", new HashMap(){{put(christina, "12345");}});
+
 		Customer john = new Customer();
 		john.addAccount(new Account("Checking", 250.0));
-		customers.put("John", john);
+		/** put username, Customer and password into the HashMap for each customer */
+		customers.put("John", new HashMap(){{put(john, "12345");}});
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		/**
+		 * Access nested HashMap to authenticate both the customer userName and password
+		 * from user input.  If this fails, program returns Null. If it passes it allows
+		 * the user to process requests.
+		 */
+
+		if ((customers.containsKey(userName) && (customers.get(userName).containsValue(password)))) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -41,15 +56,20 @@ public class NewBank {
 	public synchronized String processRequest(CustomerID customer, String request) {
 		if(customers.containsKey(customer.getKey())) {
 			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+				case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+				default : return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
-		return (customers.get(customer.getKey())).accountsToString();
+		/**
+		 * Access Customer.accountsToString() method using .keySet().iterator().next()
+		 * to source Customer key
+		 */
+		return customers.get(customer.getKey()).keySet().iterator().next().accountsToString();
+
 	}
 
 }
