9a157123cac0ff3668f6c722141bb03d07457155
chrislehrter <chris.lehrter@hotmail.co.uk>
2022-07-10 23:02:56 +0100

    Added first attempts at 'MOVE' and 'PAY' protocols

--- server/Account.java
+++ server/Account.java
@@ -7,11 +7,13 @@ public class Account {
 
 	private String accountName;
 	private double openingBalance;
+	private double currentBalance;
 	private ArrayList<TopUp> topUps;
 //create local variables for account name and opening balance within account
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
 		this.openingBalance = openingBalance;
+		this.currentBalance = openingBalance;
 		this.topUps = new ArrayList<TopUp>();
 	}
 
@@ -24,9 +26,25 @@ public class Account {
 		TopUp topUp = new TopUp(amount, new Date().toString());
 		topUps.add(topUp);
 	}
+
+	//return account balance
+	public double getCurrentBalance(){
+		return currentBalance;
+	}
+
+	//add money to the account
+	public void addBalance(double amount){
+		this.currentBalance += amount;
+	}
+
+	//remove money from the account
+	public void removeBalance(double amount){
+		this.currentBalance -= amount;
+	}
+
 //return account name and opening balance
 	public String toString() {
-		return (accountName + ": " + openingBalance);
+		return (accountName + ": " + currentBalance);
 	}
 
 }
--- server/NewBank.java
+++ server/NewBank.java
@@ -56,6 +56,10 @@ public class NewBank {
 					return topUpAccount(customer, request);
 				case "NEWACCOUNT":
 					return createNewAccount(customer, request);
+				case "MOVE":
+					return moveBetweenAccounts(customer,request);
+				case "PAY":
+					return makePayment(customer, request);
 				default:
 					return "FAIL";
 			}
@@ -85,17 +89,82 @@ public class NewBank {
 
 	// this function creates a new account
 	private String createNewAccount(CustomerID customerID, String request) {
-		Customer customer = customers.get(customerID.getKey());
 		// request is in the form of "NEWACCOUNT ACCOUNTNAME"
-		//
 		String[] tokens = request.split(" ");
 		if (tokens.length >= 2) {
+			Customer customer = customers.get(customerID.getKey());
 			String accountName = tokens[1];
 			if (customer.getAccount(accountName)!=null){
 				return "FAIL - Account already exists";
 			} else {
 				customer.addAccount(new Account(accountName,0));
 				return "Account " +accountName+ " created, opening balance "+0;
+			}
+		}
+		return "FAIL";
+	}
+
+	// This function is for members to move money from one of their accounts to another
+	// should only be allowed if:
+	// 1. Both accounts exist and are owned by the requesting (logged in) member
+	// 2. The account money is being withdrawn from has sufficient balance
+	private String moveBetweenAccounts(CustomerID customerID, String request){
+		// request is in the form of "MOVE <value> <from account name> <to account name>"
+		String[] tokens = request.split(" ");
+		if (tokens.length == 4){
+			Customer customer = customers.get(customerID.getKey());
+			double moveValue = Double.parseDouble(tokens[1]);
+			Account fromAccount = customer.getAccount(tokens[2]);
+			Account toAccount = customer.getAccount(tokens[3]);
+
+			// Only attempt if both of the accounts are found to exist and be owned by the requesting customer
+			if(fromAccount!=null && toAccount!=null){
+				if (fromAccount.getCurrentBalance() < moveValue){
+					return "FAIL - Insufficient balance in <from> account ";
+				} else {
+					//remove <moveValue> from <from> account
+					fromAccount.removeBalance(moveValue);
+					//add <moveValue> to <to> account
+					toAccount.addBalance(moveValue);
+					return "SUCCESS";
+				}
+			}
+		}
+		return "FAIL";
+	}
+
+	//allows logged in customer to make a payment to another customer
+	// NOTE: currently assumes that all users have a 'Main' account, and assumes this is where
+	//       all payments are made and received
+	private String makePayment(CustomerID senderID, String request){
+		// request is in the form of "PAY <Person/Company> <amount>"
+		String[] tokens = request.split(" ");
+
+		// input must have the correct number of arguments to try to proceed
+		if (tokens.length == 3){
+			Customer sender = customers.get(senderID.getKey());
+			CustomerID receiverID = new CustomerID(tokens[1]);
+			double payValue = Double.parseDouble(tokens[2]);
+			Account senderAccount = sender.getAccount("Main");
+
+			// if the account to make payment to can be found
+			// (for the customer/company not making the payment)
+			if(customers.containsKey(receiverID.getKey())){
+				Customer receiver = customers.get(receiverID.getKey());
+				Account receiverAccount = receiver.getAccount("Main");
+
+				//only attempts to make payment if both sending and receiving accounts can be found
+				if(senderAccount!=null && receiverAccount!=null){
+					if (senderAccount.getCurrentBalance() < payValue){
+						return "FAIL - Insufficient balance in <sender> account ";
+					} else {
+						//remove <moveValue> from <from> account
+						senderAccount.removeBalance(payValue);
+						//add <moveValue> to <to> account
+						receiverAccount.addBalance(payValue);
+						return "SUCCESS";
+					}
+				}
 			}
 		}
 		return "FAIL";
